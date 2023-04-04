8085a43aa21f705a19215cf60043343e32491b0a
BubbleWaffle <hunter13cie@gmail.com>
2023-04-04 20:27:12 +0200

    Add MouseDown events for menubar and small buttons with changes

--- SM_Audio_Player/MainWindow.xaml.cs
+++ SM_Audio_Player/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Mouse = Microsoft.VisualBasic.Devices.Mouse;
 
 namespace SM_Audio_Player
 {
@@ -24,10 +23,49 @@ namespace SM_Audio_Player
             InitializeComponent();
         }
 
+        /*Reakcja okna na użycie LMB na pasek menu*/
         private void Window_MouseDown(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-                this.DragMove();
+            /*Możliwość poruszania oknem*/
+            if (e.LeftButton == MouseButtonState.Pressed) this.DragMove();
+
+            /*Możliwość poruszania oknem oraz przywrócenie go do normalnego rozmiaru z trybu pełnego okna*/
+            if (e.LeftButton == MouseButtonState.Pressed && Application.Current.MainWindow.WindowState == WindowState.Maximized)
+            {
+                /*Pobieranie pozycji kursora*/
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+                Point mousePosition = Mouse.GetPosition(this);
+                double newX = mousePosition.X - (this.Width / 2);
+                double newY = mousePosition.Y - (this.Height / 2);
+
+                /*Sprawdzenie czy okno wychodzi poza lewą krawędź ekranu*/
+                if (newX < 0)
+                {
+                    newX = 0;
+                }
+
+                /*Sprawdzenie czy okno wychodzi poza górną krawędź ekranu*/
+                if (newY < 0)
+                {
+                    newY = 0;
+                }
+
+                /*Sprawdzenie czy okno wychodzi poza prawą krawędź ekranu*/
+                if (newX + this.Width > SystemParameters.PrimaryScreenWidth)
+                {
+                    newX = SystemParameters.PrimaryScreenWidth - this.Width;
+                }
+
+                /*Sprawdzenie czy okno wychodzi poza dolną krawędź ekranu*/
+                if (newY + this.Height > SystemParameters.PrimaryScreenHeight)
+                {
+                    newY = SystemParameters.PrimaryScreenHeight - this.Height;
+                }
+
+                /*Przypisanie pozycji lewej i górnej granicy okna*/
+                this.Left = newX;
+                this.Top = newY;
+            }
         }
     }
 }
