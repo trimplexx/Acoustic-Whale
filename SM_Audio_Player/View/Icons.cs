126d9db19598ba5ab2bcb6d2b6fc4ed5f3b701c7
Desi451 <mdesortes00@gmail.com>
2023-04-08 21:19:24 +0200

    fix to last commit

--- SM_Audio_Player/View/Icons.cs
+++ SM_Audio_Player/View/Icons.cs
@@ -18,7 +18,6 @@ namespace SM_Audio_Player.View
         }
 
         /* Ikona pause */
-
         private static readonly string stopIcon = "M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM192 160H320c17.7 0 32 14.3 32 32V320c0 17.7-14.3 32-32 32H192c-17.7 0-32-14.3-32-32V192c0-17.7 14.3-32 32-32z";
 
         public static string GetStopIcon()
@@ -27,7 +26,6 @@ namespace SM_Audio_Player.View
         }
 
         /* Ikona pełnego okna */
-
         private static readonly string maxIcon = "M.3 89.5C.1 91.6 0 93.8 0 96V224 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64V224 96c0-35.3-28.7-64-64-64H64c-2.2 0-4.4 .1-6.5 .3c-9.2 .9-17.8 3.8-25.5 8.2C21.8 46.5 13.4 55.1 7.7 65.5c-3.9 7.3-6.5 15.4-7.4 24zM48 224H464l0 192c0 8.8-7.2 16-16 16L64 432c-8.8 0-16-7.2-16-16l0-192z";
 
         public static string GetMaxIcon()
@@ -44,6 +42,8 @@ namespace SM_Audio_Player.View
 
         private static string volumeIconZero = "M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z";
 
+
+        /* Ikona dzwieku dla 0, do 50 i od 50  */
         public static string GetVolumeIconZero()
         {
             return volumeIconZero;
