4933585298680c68d4dcbf7d83dbf1abd3b47e9e
BubbleWaffle <hunter13cie@gmail.com>
2023-05-02 18:25:57 +0200

    View for custom MessageBoxes

---
+++ Acoustic-Whale/SM_Audio_Player/View/UserControls/buttons/MessageBoxOk.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SM_Audio_Player.View.UserControls.buttons;
+
+public partial class MessageBoxOk : INotifyPropertyChanged
+{
+    private string? _message;
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+    
+    public MessageBoxOk()
+    {
+        DataContext = this;
+        WarningOK.Text = "Tutej domyślny tekst konstruktora";
+        InitializeComponent();
+    }
+    
+    public string? Message
+    {
+        get => _message;
+        set
+        {
+            _message = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Message"));
+        }
+    }
+
+    private void OK_OnClick(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
